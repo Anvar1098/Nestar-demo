0b1be1d791b71c95721580bfa40f7becee55ce89
0000 <a0000@Anvars-MacBook-Air.local>
2025-04-09 17:08:39 +0900

    feat: meta tags integrations

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+          
+        {/* SEO */ }
+        <meta name="keyword" content={"nestar , nestar.uz, devex, mern, mern nestJs fullstack"} />
+        <meta name="description" content={
+          "Buy and sell properties anywhere and anytime in S.Korea |" +
+          "안녕하세요 대한민국!"
+        }/>
+      </Head>
       <body>
         <Main />
         <NextScript />
