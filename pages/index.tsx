4d2c7b79f8164db1c2c745f101700a1c3ef50e24
0000 <a0000@Anvars-MacBook-Air.local>
2025-04-11 15:46:03 +0900

    feat: publish main layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,21 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
-import { brown, green } from "@mui/material/colors";
+import { Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 
-
-const Home: NextPage = () => {
+const Name: NextPage = () => {
   return (
-    <Stack>
-        <Stack flexDirection={'column'}>
-         <Stack>
-           <Stack className="container">Popular Properties</Stack>
-         </Stack>
-
-         <Stack>
-           <Stack className="container">Top Agents</Stack>
-         </Stack>
-
-         <Stack>
-           <Stack className="container">Top Properties</Stack>
-         </Stack>
-          
-         <Stack>
-           <Stack className="container">Events</Stack>
-         </Stack>
-        </Stack>
+    <Stack className={"home-page"}>
+      <Stack>
+        <Container className="container">Popular Properties</Container>
+      </Stack>
+      <Stack>
+        <Container className="container">Top Agents</Container>
+      </Stack>
+      <Stack>
+        <Container className="container">Top Properties</Container>
+      </Stack>
     </Stack>
   );
-}
+};
 
-export default withLayoutMain(Home);
+export default withLayoutMain(Name);
